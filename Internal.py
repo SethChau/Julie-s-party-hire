67ff7451f5310de7a38715d4fb71274f14a652cd
SethChau <132409422+SethChau@users.noreply.github.com>
2023-05-22 14:05:23 +1200

    added comments and fixed some erros for Function to add an item to the list of items that are currently out and update treeview .py

--- Internal.py
+++ Internal.py
@@ -7,29 +7,35 @@ items_out = {}
 
 # Function to add an item to the list of items that are currently out and update treeview
 def add_item():
+    # Get the input values from the entry fields
     customer_name = customer_name_entry.get()
     receipt_number = receipt_number_entry.get()
     item_name = item_name_entry.get()
     item_count = item_count_entry.get()
 
+    # Check if any of the fields are empty
     if not customer_name or not receipt_number or not item_name or not item_count:
         status_label.config(text="Please fill in all fields.")
         return
 
+    # Validate the customer name input
     customer_name = customer_name_entry.get()
-    if not customer_name.isalpha():
+    if not customer_name.replace(" ", "").isalpha():
         status_label.config(text="Invalid input. Please enter a valid customer name.")
         return
 
+    # Validate the item name input
     item_name = item_name_entry.get()
-    if not item_name.isalpha():
+    if not item_name.replace(" ", "").isalpha():
         status_label.config(text="Invalid input. Please enter a valid item name.")
         return
 
+    # Validate the receipt number input
     if not receipt_number.isdigit():
-        status_label.config(text="Invalid input. Receipt number should be an number.")
+        status_label.config(text="Invalid input. Receipt number should be a number.")
         return
 
+    # Validate the item count input
     try:
         item_count = int(item_count)
     except ValueError:
@@ -40,8 +46,15 @@ def add_item():
         status_label.config(text="Invalid input. Please enter a number between 1 and 500 for item count.")
         return
 
+    # Convert the receipt number to an integer
     receipt_number = int(receipt_number)
+
+    # Add the item to the items_out dictionary
     items_out[receipt_number] = {'customer_name': customer_name, 'item_name': item_name, 'item_count': item_count}
+
+    # Update the status label
     status_label.config(
         text=f"{item_count} {item_name}(s) added to items out list for {customer_name}. Receipt number is {receipt_number}.")
-    update_treeviews()
+
+    # Call the function to update the treeview with the latest data
+    update_treeview()
